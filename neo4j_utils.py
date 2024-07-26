ded76418f2949596ea014dd37d064f2654b5738f
PRO-active <hayamatsu8810@gmail.com>
2024-07-27 08:02:51 +0900

    [WIP] コードの修正

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -1,9 +1,9 @@
 from langchain_community.graphs import Neo4jGraph
 
-def initialize_graph(graph_documents, uri, user, password):
-    graph = Neo4jGraph(uri=uri, user=user, password=password)
+def initialize_graph(graph_documents, url, user, password):
+    graph = Neo4jGraph(url=url, username=user, password=password)
     graph.add_graph_documents(graph_documents, baseEntityLabel=True, include_source=True)
     return graph
 
-def connect_to_neo4j(uri, user, password):
-    return Neo4jGraph(uri=uri, user=user, password=password)
+def connect_to_neo4j(url, user, password):
+    return Neo4jGraph(url=url, username=user, password=password)

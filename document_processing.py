ad8e66c4e162fedb291016d72360a143dbc1c9f6
PRO-active <hayamatsu8810@gmail.com>
2024-08-03 09:09:41 +0900

    [FIX] importエラー

--- document_processing.py
+++ document_processing.py
@@ -1,14 +1,14 @@
-from langchain.document_loaders import PyPDFLoader
-from typing import List
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import TokenTextSplitter
+from langchain_experimental.graph_transformers import LLMGraphTransformer
 
-def load_documents(file_path: str) -> List[str]:
-    """Load documents from a PDF file."""
-    try:
-        pdf_loader = PyPDFLoader(file_path)
-        return pdf_loader.load()
-    except Exception as e:
-        raise RuntimeError(f"Error loading {file_path}") from e
+def load_documents(file_path):
+    raw_documents = PyPDFLoader(file_path).load()
+    text_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=125)
+    documents = text_splitter.split_documents(raw_documents)
+    return documents
 
-def create_graph_documents(documents, llm):
-    llm_transformer = LLMGraphTransformer(llm=llm)
-    return llm_transformer.convert_to_graph_documents(documents)
+def create_graph_documents(documents, llm_transformer):
+    graph_documents = llm_transformer.convert_to_graph_documents(documents)
+    return graph_documents
+
